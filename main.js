b00cc988b1be2ef6e215680867d1f35128e812a5
B-AlexiaYoung <brlxyng@gmail.com>
2018-07-08 17:05:09 -0600

    image links

--- main.js
+++ main.js
@@ -1,80 +1,70 @@
 // main.js ...
 
 let projects=[
-    {   name:"React Flickr Gallery",
-        src:"./images/project11.jpg",
+    {   name: "React Flickr Gallery",
+        src: "./Images/project11.jpg",
         alt: "project eleven react flickr gallery",
         description: "Image gallery built using React and the Flickr api along with routing and a search function",
         projectUrl: "https://github.com/B-AlexiaYoung/project11-react-photo-viewer/blob/master/public/index.html",
-        projectRepo:"",
-
+        projectRepo: ""
     },
-    {   name:"Employee Directory",
-        src:" ./images/project10.jpg",
+    {   name: "Employee Directory",
+        src: "./Images/project10.jpg",
         alt: "project ten an employee directory",
         description: "An employee directory built using Javascript, html, css and an api. When an individual is clicked a modal window opens to display more details.",
         projectUrl: "./projects/project10/index.html",
-        projectRepo:"",
-
+        projectRepo: ""
     },
-    {   name:"Web App Dashboard",
-        src:" ./images/project9.jpg",
+    {   name: "Web App Dashboard",
+        src: "./Images/project9.jpg",
         alt: "project ten web app dashboard",
         description: "Built to a mockup this dashboard displays javascipt driven charts, the use of local storage and an autocompete search function.",
-        projectUrl: "projects/project9/index.html",
-        projectRepo:"",
-
+        projectUrl: "./projects/project9/index.html",
+        projectRepo: ""
     },
-    {   name:"SVG Animations",
-        src:"./images/project8.jpg",
+    {   name: "SVG Animations",
+        src: "./Images/project8.jpg",
         alt: "project eight svg animations",
         description: "This project uses optomized SVG images. Paths were used to change the appearance of the svgs and css animations and transitions were employed.",
-        projectUrl: "projects/project8/index.html",
-        projectRepo:"",
-
+        projectUrl: "./projects/project8/index.html",
+        projectRepo: ""
     },
-    {   name:"Build A Game Show App",
-        src:" ./images/project7.jpg",
+    {   name: "Build A Game Show App",
+        src: "./Images/project7.jpg",
         alt: "project seven build a game show app",
         description: "A word guessing game built using Javascript, CSS and Html. The game keeps track of correct and incorrecy choices.",
-        projectUrl: "projects/project7/index.html",
-        projectRepo:"",
-
+        projectUrl: "./projects/project7/index.html",
+        projectRepo: ""
     },
-    {   name:"Interactive Video Player",
-        src:" ./images/project6.jpg",
+    {   name: "Interactive Video Player",
+        src: "./Images/project6.jpg",
         alt: "project six interactive video player",
         description: "A video player using the mediaelement library. The controls were configured and a transcript add that hightlights as the video is played.  ",
-        projectUrl: "projects/project6/index.html",
-        projectRepo:"",
-
+        projectUrl: "./projects/project6/index.html",
+        projectRepo: ""
     },
-    {   name:"Build a Web Style Guide",
-        src:" ./images/project5.jpg",
+    {   name: "Build a Web Style Guide",
+        src: "./Images/project5.jpg",
         alt: "project five build a web style guide",
         description: "This is a sass project using partials, mixins, variables and extends. The BEM naming convention is also used.",
-        projectUrl: "projects/project5/index.html",
-        projectRepo:"",
-
+        projectUrl: "./projects/project5/index.html",
+        projectRepo: ""
     },
-    {   name:"Interactive Photo Gallery",
-        src:" ./images/project4.jpg",
+    {   name: "Interactive Photo Gallery",
+        src: "./Images/project4.jpg",
         alt: "project four interactive photo gallery",
         description: "A photo gallery built using Javascript, and Jquery.  It includes  a search funtion that displays only matched results.",
-        projectUrl: "projects/project4/index.html",
-        projectRepo:"",
-
+        projectUrl: "./projects/project4/index.html",
+        projectRepo: ""
     },
-    {   name:"Online Registration Form",
-        src:" ./images/project3.jpg",
+    {   name: "Online Registration Form",
+        src: "./Images/project3.jpg",
         alt: "project three online registration form",
         description: "A form built to a mockup using html and CSS. The form contains required and placeholder attributes.",
-        projectUrl: "projects/project3/index.html",
-        projectRepo:"",
-
+        projectUrl: "./projects/project3/index.html",
+        projectRepo: ""
     },
     
-
 ]
 let displayContainer= document.getElementById("displayContainer");
 // loop through projects array to display photos.
